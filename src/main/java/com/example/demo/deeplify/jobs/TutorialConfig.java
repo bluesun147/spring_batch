2865d81af885126e8e95b793e7a576c9acb6269e
Haechan Kim <bluesun147@naver.com>
2023-09-06 13:48:21 +0900

    [Chore] 폴더 변경

---
+++ src/main/java/com/example/demo/deeplify/jobs/TutorialConfig.java
@@ -0,0 +1,33 @@
+package com.example.demo.deeplify.jobs;
+
+import com.example.demo.deeplify.tasklets.TutorialTasklet;
+import lombok.RequiredArgsConstructor;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// https://deeplify.dev/back-end/spring/batch-tutorial
+
+@Configuration
+@RequiredArgsConstructor
+public class TutorialConfig {
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+    @Bean
+    public Job tutorialJob() {
+        return jobBuilderFactory.get("tutorialJob")
+                .start(tutorialStep())
+                .build();
+    }
+
+    @Bean
+    public Step tutorialStep() {
+        return stepBuilderFactory.get("tutorialStep")
+                .tasklet(new TutorialTasklet())
+                .build();
+    }
+}
