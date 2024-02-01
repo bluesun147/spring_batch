377f37eb9d937b6fa6062616dbbba8f434997e08
bluesun147 <bluesun147@naver.com>
2024-02-01 21:10:06 +0900

    [Delete] 충돌로 인한 이전 학습용 코드 삭제

--- src/main/java/com/example/demo/csvSchedule/repository/ScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.csvSchedule.repository;
-
-import com.example.demo.csvSchedule.entity.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-}
--- src/main/java/com/example/demo/poi/repository/UserPointRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.poi.repository;
-
-import com.example.demo.poi.entity.UserPoint;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPointRepository extends JpaRepository<UserPoint, Long> {
-}
